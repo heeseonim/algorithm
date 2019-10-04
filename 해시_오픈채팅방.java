f95c5337a1ce638a432a6574b94010928a6dc858
heeseonim <52234358+heeseonim@users.noreply.github.com>
2019-10-04 13:39:59 +0900

    Create 해시_오픈채팅방.java

---
+++ 해시_오픈채팅방.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class KAKAO_오픈채팅방 {
+	public static void main(String[] args) {
+		String[] record = { "Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo",
+				"Change uid4567 Ryan" };
+		System.out.println(Arrays.toString(solution(record)));
+	}
+
+	public static String[] solution(String[] record) {
+		List<String[]> commandList = new ArrayList<>();
+		Map<String, String> userInfo = new HashMap<>();
+		
+		for (int i = 0; i < record.length; i++) {
+			String[] com = record[i].split(" ");
+			if (com[0].equals("Enter")) {
+				userInfo.put(com[1], com[2]);
+				commandList.add(com);
+			} else if (com[0].equals("Leave")) {
+				commandList.add(com);
+			} else {
+				userInfo.put(com[1], com[2]);
+			}
+		}
+		
+		String[] answer = new String[commandList.size()];
+		int index=0;
+		
+		for(String[] c : commandList) {
+			if (c[0].equals("Enter")) {
+				answer[index++] = userInfo.get(c[1]) + "님이 들어왔습니다.";
+			} else {
+				answer[index++] = userInfo.get(c[1]) + "님이 나갔습니다.";
+			}
+		}
+		
+		return answer;
+	}
+}
