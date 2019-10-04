5691c4dcd7313e29f2bb96e706a7ed64683f7e4c
heeseonim <52234358+heeseonim@users.noreply.github.com>
2019-10-04 13:23:13 +0900

    Update and rename 순열_백준_10974_모든순열.java to 순열_모든순열.java

--- 순열_모든순열.java
+++ 순열_모든순열.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Main_백준_10974_모든순열 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
