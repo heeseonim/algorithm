d1baeaf27541098d2b6f38969b4d194f0c1545b5
heeseonim <52234358+heeseonim@users.noreply.github.com>
2019-10-04 13:23:55 +0900

    Update and rename 백트래킹_백준_1987_알파벳.java to 백트래킹_알파벳.java

--- 백트래킹_알파벳.java
+++ 백트래킹_알파벳.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_백준_1987_알파벳 {
 	public static int R, C;
 	public static char[][] map;
 	public static boolean[] visitAlpha;
