6d692ae4a488b4acc18b0c58aa8e559e7b8e3238
heeseonim <52234358+heeseonim@users.noreply.github.com>
2019-10-04 13:24:15 +0900

    Update and rename DP_백준_9465_스티커.java to DP_스티커.java

--- DP_스티커.java
+++ DP_스티커.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_백준_9465_스티커 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
