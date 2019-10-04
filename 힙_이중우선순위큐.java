791fe9fc58c878b8784d6868e950e58b09ee779b
heeseonim <52234358+heeseonim@users.noreply.github.com>
2019-10-04 13:36:44 +0900

    Create 힙_이중우선순위큐.java

---
+++ 힙_이중우선순위큐.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class 힙_이중우선순위큐 {
+	public static void main(String[] args) {
+		String[] operations = { "I 7", "I 5", "I -5", "D -1" };
+		System.out.println(Arrays.toString(solution(operations)));
+	}
+
+	public static int[] solution(String[] operations) {
+		int[] answer = {0, 0};
+		// 오름차순 우선순위 큐
+		PriorityQueue<Integer> q = new PriorityQueue<>();
+		// 내림차순 우선순위 큐
+		PriorityQueue<Integer> qReverse = new PriorityQueue<>(Comparator.reverseOrder());
+		int max = 0, min = 0;
+		for (int i = 0; i < operations.length; i++) {
+			String[] token = operations[i].split(" ");
+			if (token[0].equals("I")) {
+				q.add(Integer.parseInt(token[1]));
+				qReverse.add(Integer.parseInt(token[1]));
+			} else { // D
+				if (!q.isEmpty()) {
+					if (token[1].equals("1")) { // 최댓값 제거
+						max = qReverse.peek();
+						q.remove(max);
+						qReverse.remove(max);
+					} else { // -1, 최솟값 제거
+						min = q.peek();
+						q.remove(min);
+						qReverse.remove(min);
+					}
+				}
+			}
+		}
+		
+		if(!q.isEmpty()) {
+			answer[0] = qReverse.peek();
+			answer[1] = q.peek();
+		}
+
+		return answer;
+	}
+}
