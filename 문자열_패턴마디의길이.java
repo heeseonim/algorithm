4d9aec279c5f0ca0e0a5264e06feeacf6a47252f
heeseonim <52234358+heeseonim@users.noreply.github.com>
2019-10-07 11:30:18 +0900

    Create 문자열_패턴마디의길이.java

---
+++ 문자열_패턴마디의길이.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution_2007_패턴마디의길이 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			String str = br.readLine();
+			int count = Integer.MAX_VALUE;
+			for (int i = 1; i <= 10; i++) {
+				if (str.substring(0, i).equals(str.substring(i, 2 * i))) {
+					count = Math.min(count, i);
+				}
+			}
+			System.out.println("#" + tc + " " + count);
+		}
+	} // end of main
+} // end of class
