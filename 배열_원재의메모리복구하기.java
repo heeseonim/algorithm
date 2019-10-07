61b01eaa6913d59a2150f2364d5cb1174b0fa2c0
heeseonim <52234358+heeseonim@users.noreply.github.com>
2019-10-07 11:38:18 +0900

    Create 배열_원재의메모리복구하기.java

---
+++ 배열_원재의메모리복구하기.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Solution_1289_원재의메모리복구하기 {
+
+	public static void main(String[] args) {
+		int T;
+		String num = "";
+		int[] bit = new int[51];
+		Scanner scan = new Scanner(System.in);
+
+		T = scan.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			num = scan.next();
+
+			for (int i = 0; i < num.length(); i++) {
+				bit[i] = num.charAt(i) - 48;
+			}
+
+			int cnt = 0;
+			for (int i = 0; i < num.length(); i++) {
+				if (bit[i] == 1) {
+					cnt++;
+					for (int j = i + 1; j < num.length(); j++) {
+						if (bit[j] == 1)
+							cnt--;
+						else
+							cnt++;
+						break;
+					}
+				}
+			}
+			System.out.printf("#%d %d\n", tc + 1, cnt);
+		}
+	}
+}
