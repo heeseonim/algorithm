f023726639b47b888363d44450f829191c2d046a
heeseonim <52234358+heeseonim@users.noreply.github.com>
2019-10-31 17:13:20 +0900

    Update and rename 배열_준홍이의카드놀이.java to Solution_SWEA_7102_준홍이의카드놀이.java

--- Solution_SWEA_7102_준홍이의카드놀이.java
+++ Solution_SWEA_7102_준홍이의카드놀이.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution_7102_준홍이의카드놀이 {
+public class Solution_SWEA_7102_준홍이의카드놀이 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
