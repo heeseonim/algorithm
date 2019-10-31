f79b2c0462134212cedc2e81add017cdc100262e
heeseonim <52234358+heeseonim@users.noreply.github.com>
2019-10-31 17:12:33 +0900

    Update and rename 배열_재관이의대량할인.java to Solution_SWEA_4050_재관이의대량할인.java

--- Solution_SWEA_4050_재관이의대량할인.java
+++ Solution_SWEA_4050_재관이의대량할인.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Solution_4050 {
+public class Solution_SWEA_4050_재관이의대량할인 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
