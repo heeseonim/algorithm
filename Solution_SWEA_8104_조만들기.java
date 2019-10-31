d0a68d96690f8f727a6d777a96db74220146a29a
heeseonim <52234358+heeseonim@users.noreply.github.com>
2019-10-31 17:20:34 +0900

    Update and rename 배열_조만들기.java to Solution_SWEA_8104_조만들기.java

--- Solution_SWEA_8104_조만들기.java
+++ Solution_SWEA_8104_조만들기.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution_8104_조만들기 {
+public class Solution_SWEA_8104_조만들기 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
