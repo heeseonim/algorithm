95c0cdc9282bb0ec1e4bb346feb79cf9227ddea2
heeseonim <52234358+heeseonim@users.noreply.github.com>
2019-10-31 17:10:21 +0900

    Update and rename 플로이드와샬_사람네트워크.java to Solution_SWEA_1263_사람네트워크.java

--- Solution_SWEA_1263_사람네트워크.java
+++ Solution_SWEA_1263_사람네트워크.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution_1263 {
+public class Solution_SWEA_1263_사람네트워크 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
