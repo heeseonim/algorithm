ccf453ee445269c8b4826aced00f5a0987ebccd1
heeseonim <52234358+heeseonim@users.noreply.github.com>
2019-10-31 17:19:13 +0900

    Update and rename 배열_탑.java to Main_정올_1809_탑.java

--- Main_정올_1809_탑.java
+++ Main_정올_1809_탑.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_1809 {
+public class Main_정올_1809_탑 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
