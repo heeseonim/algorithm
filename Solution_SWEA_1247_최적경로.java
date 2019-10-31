3f8a78026c0255f6f3aa3846e88ff85780fe4343
heeseonim <52234358+heeseonim@users.noreply.github.com>
2019-10-31 17:30:46 +0900

    Update and rename 순열_최적경로.java to Solution_SWEA_1247_최적경로.java

--- Solution_SWEA_1247_최적경로.java
+++ Solution_SWEA_1247_최적경로.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution_1247_최적경로 {
+public class Solution_SWEA_1247_최적경로 {
 	static class Point {
 		int x;
 		int y;
