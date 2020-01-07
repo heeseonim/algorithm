4890a6891f449396eb559457603c9023d9fff886
heeseonim <hisundl@naver.com>
2020-01-07 11:48:45 +0900

    a

--- BaekjoonOnlineJudge/Main_백준_1463_1로만들기.java
+++ BaekjoonOnlineJudge/Main_백준_1463_1로만들기.java
@@ -15,7 +15,7 @@ public class Main_1463 {
 	// Top-down
 	public static void check(int level, int cur) {
 		if (level >= result)
-			return; // °¡ÁöÄ¡±â
+			return; // ê°€ì§€ì¹˜ê¸°
 
 		if (cur == 1) {
 			if (level < result)
